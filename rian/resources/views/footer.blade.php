5fd697e61f68b9a68ca7688ebebf26aa0b02f36d
egi <egijaelani1010@gmail.com>
2026-04-03 13:15:16 +0700

    pengumpulan quis

--- rian/resources/views/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-gray-800 text-white text-center p-4 mt-10">
-    <p class="text-sm">© 2026 MyWeb | All Rights Reserved</p>
-</footer>
--- rian/resources/views/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="bg-blue-600 text-white shadow-md">
-    <div class="container mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 class="text-lg font-bold">MyWeb</h1>
-        <div class="space-x-4">
-            <a href="/" class="hover:text-gray-200">Home</a>
-            <a href="#" class="hover:text-gray-200">About</a>
-        </div>
-    </div>
-</nav>
